b2238d3b308c8f619a955e51dc86860e084506bf
nitis-saini <nitish.saini6330@gmail.com>
2024-09-18 11:46:27 +0530

    updated

---
+++ class/LEC-15/data_type.js
@@ -0,0 +1,27 @@
+let a=10;
+let b=-4;
+let c= 2.5;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+// 2. string
+
+let name='nitish';
+console.log(typeof(name));
+
+// 3. boolean
+let isadmin=true;
+console.log(typeof(isadmin));
+
+// 4.undefined
+let y;
+console.log(typeof(y));
+y=20;
+console.log(typeof(y));
+
+// 5 .NULL
+let z=null;
+console.log(typeof(z));
+
+
---
+++ class/LEC-15/script1.js
@@ -0,0 +1,13 @@
+/*var a=10;
+console.log(a)
+var a=20;
+console.log(a)
+a=30;
+console.log(a)*/
+
+/*let a=10;
+console.log(a)*/
+
+const a=10;
+console.log(a)
+
